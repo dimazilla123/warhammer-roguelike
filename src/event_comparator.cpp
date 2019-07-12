3b80bc02c3c775ba685553fed52eeb1fd9b77217
Dimitius <evtushenko.2002@mail.ru>
2019-07-12 14:03:35 +0300

    control fix

--- src/event_comparator.cpp
+++ src/event_comparator.cpp
@@ -1,9 +1,11 @@
 #include "event_comparator.h"
 #include "control_event.h"
-#include "control_event.h"
-#include "control_event.h"
 
 bool EventComparator::operator()(Event *a, Event *b) const
 {
+    ControlEvent *a_is_control = dynamic_cast<ControlEvent*>(a);
+    ControlEvent *b_is_control = dynamic_cast<ControlEvent*>(b);
+    if ((a_is_control && b_is_control) || (!a_is_control && !b_is_control))
+        return (b_is_control == nullptr);
     return a->getTime() > b->getTime();
 }
